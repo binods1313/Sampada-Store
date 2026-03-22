7ae6baaa2f3b6bb7950b4eed8992475974e1ae32
Dibyasini Triya <128450320+DibyasiniTriya@users.noreply.github.com>
2026-03-23 04:06:35 +0530

    debug: restore full _app.js to test

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,56 @@
-// pages/_app.js - DEBUG: Adding back providers one by one
+// pages/_app.js - DEBUG: Adding back ALL original providers
 import '../styles/globals.css';
+import { Layout, ErrorMonitorDemo, TestSuiteNavigator, EnhancedErrorHandlerNavigator, ErrorBoundaryWithFallback, ImageOptimizerTestNavigator } from '../components';
+import { OfflineWrapper } from '../components';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 import { UIProvider } from '../context/StateContext';
 import { CartProvider } from '../context/CartContext';
+import { WishlistProvider } from '../components/WishlistSystem';
+import dynamic from 'next/dynamic';
 
-// TEMP: Comment out problematic ones
-// import { Layout, ErrorMonitorDemo, TestSuiteNavigator, EnhancedErrorHandlerNavigator, ErrorBoundaryWithFallback, ImageOptimizerTestNavigator } from '../components';
-// import { OfflineWrapper } from '../components';
-// import { WishlistProvider } from '../components/WishlistSystem';
-// import dynamic from 'next/dynamic';
+// Dynamically import SampadaVoiceButton to avoid SSR issues with Web Speech API
+const SampadaVoiceButton = dynamic(
+  () => import('../components/VoiceAssistant/SampadaVoiceButton'),
+  { ssr: false }
+);
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <UIProvider>
-        <CartProvider>
-          <Toaster position="bottom-center" />
-          <Component {...pageProps} />
-        </CartProvider>
-      </UIProvider>
-    </SessionProvider>
+    <ErrorBoundaryWithFallback>
+      <SessionProvider session={session}>
+        <UIProvider>
+          <CartProvider>
+            <WishlistProvider>
+              <OfflineWrapper>
+                <Layout>
+                  <Toaster
+                    position="bottom-center"
+                    toastOptions={{
+                      style: {
+                        background: '#333',
+                        color: '#fff',
+                      }
+                    }}
+                  />
+                  <Component {...pageProps} />
+                  {/* Sampada AI Voice Assistant */}
+                  <SampadaVoiceButton />
+                  {/* Error Monitor for Development */}
+                  <ErrorMonitorDemo />
+                  {/* Test Suite Navigator for Development */}
+                  <TestSuiteNavigator />
+                  {/* Enhanced Error Handler Navigator for Development */}
+                  <EnhancedErrorHandlerNavigator />
+                  {/* Image Optimizer Test Navigator for Development */}
+                  <ImageOptimizerTestNavigator />
+                </Layout>
+              </OfflineWrapper>
+            </WishlistProvider>
+          </CartProvider>
+        </UIProvider>
+      </SessionProvider>
+    </ErrorBoundaryWithFallback>
   );
 }
 
