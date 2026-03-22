64660d3614bfe3da59f6e6d3c6c8709ba290a57b
Dibyasini Triya <128450320+DibyasiniTriya@users.noreply.github.com>
2026-03-23 04:04:27 +0530

    debug: minimal _app.js - comment out all providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,62 +1,22 @@
-// 2. Update your _app.js file to wrap your application with both providers:
-// pages/_app.js
+// pages/_app.js - DEBUG MINIMAL
 import '../styles/globals.css';
-import '../styles/product-description.css';
-import '../styles/footer-banner-description.css';
-import '../styles/About.module.css';
-import '../styles/product-variant.css';
-import '../styles/ProductCard.css';
-import { Layout, ErrorMonitorDemo, TestSuiteNavigator, EnhancedErrorHandlerNavigator, ErrorBoundaryWithFallback, ImageOptimizerTestNavigator } from '../components';
-import { OfflineWrapper } from '../components';
-import { Toaster } from 'react-hot-toast';
-import { SessionProvider } from 'next-auth/react';
-import { UIProvider } from '../context/StateContext';
-import { CartProvider } from '../context/CartContext';
-import { WishlistProvider } from '../components/WishlistSystem';
-import dynamic from 'next/dynamic';
 
-// Dynamically import SampadaVoiceButton to avoid SSR issues with Web Speech API
-const SampadaVoiceButton = dynamic(
-  () => import('../components/VoiceAssistant/SampadaVoiceButton'),
-  { ssr: false }
-);
+// TEMP: Comment out ALL imports to isolate the issue
+// import { Layout, ErrorMonitorDemo, TestSuiteNavigator, EnhancedErrorHandlerNavigator, ErrorBoundaryWithFallback, ImageOptimizerTestNavigator } from '../components';
+// import { OfflineWrapper } from '../components';
+// import { Toaster } from 'react-hot-toast';
+// import { SessionProvider } from 'next-auth/react';
+// import { UIProvider } from '../context/StateContext';
+// import { CartProvider } from '../context/CartContext';
+// import { WishlistProvider } from '../components/WishlistSystem';
+// import dynamic from 'next/dynamic';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+// TEMP: Minimal app
+function MyApp({ Component, pageProps }) {
   return (
-    <ErrorBoundaryWithFallback>
-      <SessionProvider session={session}>
-        <UIProvider>
-          <CartProvider>
-            <WishlistProvider>
-              <OfflineWrapper>
-                <Layout>
-                  <Toaster
-                    position="bottom-center"
-                    toastOptions={{
-                      style: {
-                        background: '#333',
-                        color: '#fff',
-                      }
-                    }}
-                  />
-                  <Component {...pageProps} />
-                  {/* Sampada AI Voice Assistant */}
-                  <SampadaVoiceButton />
-                  {/* Error Monitor for Development */}
-                  <ErrorMonitorDemo />
-                  {/* Test Suite Navigator for Development */}
-                  <TestSuiteNavigator />
-                  {/* Enhanced Error Handler Navigator for Development */}
-                  <EnhancedErrorHandlerNavigator />
-                  {/* Image Optimizer Test Navigator for Development */}
-                  <ImageOptimizerTestNavigator />
-                </Layout>
-              </OfflineWrapper>
-            </WishlistProvider>
-          </CartProvider>
-        </UIProvider>
-      </SessionProvider>
-    </ErrorBoundaryWithFallback>
+    <>
+      <Component {...pageProps} />
+    </>
   );
 }
 
