83263832fc5bd6e451cb3c9aaddb2b3f3e277d02
Dibyasini Triya <128450320+DibyasiniTriya@users.noreply.github.com>
2026-03-23 04:06:20 +0530

    debug: add back basic providers (SessionProvider, UIProvider, CartProvider, Toaster)

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,26 @@
-// pages/_app.js - DEBUG MINIMAL
+// pages/_app.js - DEBUG: Adding back providers one by one
 import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+import { UIProvider } from '../context/StateContext';
+import { CartProvider } from '../context/CartContext';
 
-// TEMP: Comment out ALL imports to isolate the issue
+// TEMP: Comment out problematic ones
 // import { Layout, ErrorMonitorDemo, TestSuiteNavigator, EnhancedErrorHandlerNavigator, ErrorBoundaryWithFallback, ImageOptimizerTestNavigator } from '../components';
 // import { OfflineWrapper } from '../components';
-// import { Toaster } from 'react-hot-toast';
-// import { SessionProvider } from 'next-auth/react';
-// import { UIProvider } from '../context/StateContext';
-// import { CartProvider } from '../context/CartContext';
 // import { WishlistProvider } from '../components/WishlistSystem';
 // import dynamic from 'next/dynamic';
 
-// TEMP: Minimal app
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    <SessionProvider session={session}>
+      <UIProvider>
+        <CartProvider>
+          <Toaster position="bottom-center" />
+          <Component {...pageProps} />
+        </CartProvider>
+      </UIProvider>
+    </SessionProvider>
   );
 }
 
